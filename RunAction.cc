65be5d7c1e3c1a1fbd303be37e7e35494c1a6171
CharlesJosephNaim <127447519+CharlesJosephNaim@users.noreply.github.com>
2025-10-01 10:51:00 -0400

    Update RunAction.cc

--- RunAction.cc
+++ RunAction.cc
@@ -10,7 +10,6 @@ RunAction::~RunAction() {}
 void RunAction::BeginOfRunAction(const G4Run*)
 {
     G4cout << "### Run started ###" << G4endl;
-    // Optionnel: réinitialisation ou préparation
 }
 
 void RunAction::EndOfRunAction(const G4Run*)
